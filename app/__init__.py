2c6090f6d45db2504bd5a08d6c0bf42b12890e3f
Bolaji <bjwealthy@gmail.com>
2019-05-30 12:14:03 +0100

    latest commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 #__name__ is a python predefined variable that is passed to the name of the module in which it is used
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-#passing __name__ will configure Flask the correct way
-from app import routes  #route is imported at the bottom to avoid circular imports
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
+#route is imported at the bottom to avoid circular imports
